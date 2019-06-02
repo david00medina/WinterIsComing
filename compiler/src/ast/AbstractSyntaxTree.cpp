f179dfbd7d2226bdf786213ac6ce3c43b5cb7a06
David Alberto Medina Medina <david00medina@gmail.com>
2019-06-02 11:04:57 +0100

    [+] Nuevos métodos para la clase AbstractSyntaxTree

--- compiler/src/ast/AbstractSyntaxTree.cpp
+++ compiler/src/ast/AbstractSyntaxTree.cpp
@@ -6,19 +6,32 @@ namespace wic
                                               ASTNode* ptr1, ASTNode** ptr2, ASTNode* ptr3, ASTNode** args,
                                               GSymbolTable* gst, SSymbolTable* sst, LSymbolTable* lst)
 
-        {
-            node->id = id;
-            node->node_t = node_t;
-            node->data_t = data_t;
-            node->data_v = data_v;
-            node->ptr1 = ptr1;
-            node->ptr2 = ptr2;
-            node->ptr3 = ptr3;
-            node->args = args;
-            node->gst = gst;
-            node->sst = sst;
-            node->lst = lst;
+    {
+        node->id = id;
+        node->node_t = node_t;
+        node->data_t = data_t;
+        node->data_v = data_v;
+        node->ptr1 = ptr1;
+        node->ptr2 = ptr2;
+        node->ptr3 = ptr3;
+        node->args = args;
+        node->gst = gst;
+        node->sst = sst;
+        node->lst = lst;
 
-            return node;
-        }
+        return node;
+    }
+
+
+    void AbstractSyntaxTree::connect(ASTNode * ptr1, ASTNode** ptr2, ASTNode* ptr3)
+    {
+        node->ptr1 = ptr1;
+        node->ptr2 = ptr2;
+        node->ptr3 = ptr3;
+    }
+
+    void AbstractSyntaxTree::add_args(wic::ASTNode** args)
+    {
+        node->args = args;
+    }
 }
